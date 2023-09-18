f3268ab356719bb33261f60ba3e05de49496b8fd
Arunava-27 <drarunkundu22@gmail.com>
2023-09-18 19:06:18 +0530

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+// hello world
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Hell " << name << "!" << endl;
+
+    return 0;
+}
